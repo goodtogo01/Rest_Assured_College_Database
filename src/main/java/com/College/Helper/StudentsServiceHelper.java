289398546ddaa6c495392183ec14ca0ab2ff34ef
kz1985 <khosruzzaman.ny@gmail.com>
2023-10-20 12:39:23 -0400

    rest added

--- src/main/java/com/College/Helper/StudentsServiceHelper.java
+++ src/main/java/com/College/Helper/StudentsServiceHelper.java
@@ -1,6 +1,16 @@
 package com.College.Helper;
 
+import com.College.Constants.EndPoints;
+import com.College.Model.Students;
 import com.College.Utils.ConfigManager;
+import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import jdk.internal.org.objectweb.asm.TypeReference;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class StudentsServiceHelper {
 	/*
@@ -15,7 +25,18 @@ public class StudentsServiceHelper {
 	
 
 	public StudentsServiceHelper() {
-		
-		
+		RestAssured.baseURI=BASE_URL;
+		RestAssured.port=Integer.parseInt(PORT);
+		RestAssured.useRelaxedHTTPSValidation();
+	}
+	public List<Students> getAllStudents(){
+		Response response = RestAssured.given().contentType(ContentType.JSON)
+				.get(EndPoints.GET_ALL_STUDENTS)
+				.andReturn();
+
+		Type type = new TypeReference<List<Students>>(){}.getType();
+		List<Students> studentsList = response.as(type);
+		return studentsList;
+
 	}
 }
